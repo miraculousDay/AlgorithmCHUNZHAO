8faddf74b723d8ecc61f50427e5e190d02340010
miraculousDay <1024552140@qq.com>
2021-01-27 17:57:18 +0800

    前序遍历内容提交

--- Week_02/homework/InorderTraversal.java
+++ Week_02/homework/InorderTraversal.java
@@ -14,13 +14,77 @@ import java.util.Stack;
  *
  * 2.遍历，手动维护一个栈，来处理中序遍历的顺序
  * @see #inorderTraversal_2(TreeNode)
- * 
+ *
+ * 3.莫里斯中序遍历
+ * @see #inorderTraversal_3(TreeNode)
+ *
  * @author : tanyu
  * create at:  2021-01-26 15:51
  * @description: 二叉树的中序遍历
  */
 public class InorderTraversal {
 
+    /**
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(1)
+     *
+     * 处理逻辑：
+     * 1.先判断当前节点是否有左节点，没有则将当前节点值方加入结果，将节点指向右侧节点
+     * 2.第1步中存在左节点，则找到左边树x的最右边的节点，记做predecessor，
+     * 如果predecessor的右节点为空，则predecessor的右节点指向x，x则指向左节点。
+     * 如果predecessor的右节点不为空，将x的值加入结果，x则指向右节点。predecessor的右节点则置位空
+     *
+     * @param root
+     * @return
+     */
+    public List<Integer> inorderTraversal_3(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        // 指示节点
+        TreeNode predecessor = null;
+        while (root != null) {
+            // 判断左子树是否存在
+            if (root.left != null) {
+                predecessor = root.left;
+                // 找到当前节点最右的节点，predecessor.
+                // right != root判断表示predecessor是新的predecessor节点
+                while (predecessor.right != null && predecessor.right != root) {
+                    predecessor = predecessor.right;
+                }
+                // 新predecessor
+                if (predecessor.right == null) {
+                    // predecessor右子树指向root
+                    predecessor.right = root;
+                    // 继续往左子树遍历
+                    root = root.left;
+                } else {
+                    // 旧的predecessor，则将root值加入结果
+                    result.add(root.val);
+                    root = root.right;
+                    // 将破坏树结构的连接点去掉
+                    predecessor.right = null;
+                }
+            } else {
+                //没有左子树则将则值加入结果，并指向右节点
+                result.add(root.val);
+                root = root.right;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(n)
+     *
+     * 手动维护一个栈，
+     * 1.依次将根节点、左子树、左子树跟节点全部入栈
+     * 2.然后取出最底部的左节点，获取该节点的值，
+     * 3.获取相应的跟节点，获取该根节点的值
+     * 4.获取3位置的右子树，当右子树存在左右子树(即为子树跟节点，则循环1、2、2的操作)
+     *
+     * @param root
+     * @return
+     */
     public List<Integer> inorderTraversal_2(TreeNode root) {
         List<Integer> result = new ArrayList<>();
         Stack<TreeNode> stack = new Stack<>();
@@ -31,7 +95,7 @@ public class InorderTraversal {
             }
             root = stack.pop();
             result.add(root.val);
-            root  = root.right;
+            root = root.right;
         }
         return result;
     }
---
+++ Week_02/homework/PreorderTraversal.java
@@ -0,0 +1,102 @@
+package homework;
+
+import javax.print.DocFlavor;
+import javax.swing.plaf.synth.SynthTextAreaUI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * LeetCode链接：
+ * https://leetcode-cn.com/problems/binary-tree-preorder-traversal/
+ *
+ * 解题思路：
+ * 1.递归
+ * @see #preorderTraversal_1(InorderTraversal.TreeNode)
+ *
+ * 2.迭代，使用stack维护前序遍历顺序s
+ * @see #preorderTraversal_2(InorderTraversal.TreeNode)
+ *
+ * 3.迭代2
+ * @see #preorderTraversal_3(InorderTraversal.TreeNode)
+ *
+ * @author : tanyu
+ * create at:  2021-01-27 16:14
+ * @description: 二叉树的前序遍历
+ */
+public class PreorderTraversal {
+
+    /**
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(n)
+     *
+     * @param root
+     * @return
+     */
+    public List<Integer> preorderTraversal_3(InorderTraversal.TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Stack<InorderTraversal.TreeNode> stack = new Stack<>();
+        // 预先往栈中放入根节点
+        stack.push(root);
+        while (!stack.empty()) {
+            // 获取根节点
+            root = stack.pop();
+            if (root != null) {
+                // 根据栈的先进后出的特性，将左节点后进站
+                result.add(root.val);
+                stack.push(root.right);
+                stack.push(root.left);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(n)
+     *
+     * 与中序遍历类似，差别点是将值加入结果的位置。
+     *
+     * @param root
+     * @return
+     */
+    public List<Integer> preorderTraversal_2(InorderTraversal.TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Stack<InorderTraversal.TreeNode> stack = new Stack<>();
+        while (root != null || !stack.empty()) {
+            while (root != null) {
+                result.add(root.val);
+                stack.push(root.left);
+                root = root.left;
+            }
+            root = stack.pop();
+            root = root.right;
+        }
+        return result;
+    }
+
+    /**
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(n)
+     *
+     * @param root
+     * @return
+     */
+    public List<Integer> preorderTraversal_1(InorderTraversal.TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        helper(result, root);
+        return result;
+    }
+
+    public void helper(List<Integer> result, InorderTraversal.TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        result.add(root.val);
+        helper(result, root.left);
+        helper(result, root.right);
+    }
+}
